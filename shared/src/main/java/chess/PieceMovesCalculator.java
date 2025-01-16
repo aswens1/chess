49e3a4ef560c83fa5387af6c8451b96cc9164334
alyseswenson <alyse.swenson@gmail.com>
2025-01-16 11:30:51 -0700

    initialised type, start position, end position, board

--- shared/src/main/java/chess/PieceMovesCalculator.java
+++ shared/src/main/java/chess/PieceMovesCalculator.java
@@ -2,8 +2,16 @@ package chess;
 
 public class PieceMovesCalculator {
 
-    public PieceMovesCalculator(ChessPiece.PieceType pieceType, ChessPosition startPosition,
-                                ChessPosition endPosition, ChessBoard chessBoard) {
+    private ChessPiece.PieceType pieceType;
+    private ChessPosition startPosition;
+    private ChessPosition endPosition;
+    private ChessBoard chessBoard;
 
+    public PieceMovesCalculator(ChessPiece.PieceType type, ChessPosition startPos,
+                                ChessPosition endPos, ChessBoard board) {
+        this.pieceType = type;
+        this.startPosition = startPos;
+        this.endPosition = endPos;
+        this.chessBoard = board;
     }
 }
