551fb13669ab75377e001c2e2bab98d2145754d0
alyseswenson <alyse.swenson@gmail.com>
2025-01-16 11:26:59 -0700

    PieceMovesCalculator

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -11,7 +11,12 @@ import java.util.Collection;
  */
 public class ChessPiece {
 
+    private ChessGame.TeamColor teamColor;
+    private ChessPiece.PieceType pieceType;
+
     public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
+        this.teamColor = pieceColor;
+        this.pieceType = type;
     }
 
     /**
@@ -30,14 +35,14 @@ public class ChessPiece {
      * @return Which team this chess piece belongs to
      */
     public ChessGame.TeamColor getTeamColor() {
-        throw new RuntimeException("Not implemented");
+        return teamColor;
     }
 
     /**
      * @return which type of chess piece this piece is
      */
     public PieceType getPieceType() {
-        throw new RuntimeException("Not implemented");
+        return pieceType;
     }
 
     /**
---
+++ shared/src/main/java/chess/PieceMovesCalculator.java
@@ -0,0 +1,9 @@
+package chess;
+
+public class PieceMovesCalculator {
+
+    public PieceMovesCalculator(ChessPiece.PieceType pieceType, ChessPosition startPosition,
+                                ChessPosition endPosition, ChessBoard chessBoard) {
+
+    }
+}
