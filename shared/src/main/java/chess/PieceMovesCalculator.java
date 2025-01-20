59ebf0cb7cd1bac86fcdf97d153e823af5bb06e3
alyseswenson <alyse.swenson@gmail.com>
2025-01-19 19:03:54 -0700

    started an interface for PieceMovesCalculator, generated code for the others

--- shared/src/main/java/chess/PieceMovesCalculator.java
+++ shared/src/main/java/chess/PieceMovesCalculator.java
@@ -1,17 +1,10 @@
 package chess;
 
-public class PieceMovesCalculator {
-
-    private ChessPiece.PieceType pieceType;
-    private ChessPosition startPosition;
-    private ChessPosition endPosition;
-    private ChessBoard chessBoard;
-
-    public PieceMovesCalculator(ChessPiece.PieceType type, ChessPosition startPos,
-                                ChessPosition endPos, ChessBoard board) {
-        this.pieceType = type;
-        this.startPosition = startPos;
-        this.endPosition = endPos;
-        this.chessBoard = board;
-    }
+public interface PieceMovesCalculator {
+    public void king();
+    public void queen();
+    public void knight();
+    public void bishop();
+    public void rook();
+    public void pawn();
 }
