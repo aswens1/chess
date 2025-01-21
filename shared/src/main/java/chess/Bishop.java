5f04db257d5db6b10fa13046da01e8e9c6697bb9
alyseswenson <alyse.swenson@gmail.com>
2025-01-21 11:43:49 -0700

    passed first bishop move test  - move to edge

--- shared/src/main/java/chess/Bishop.java
+++ shared/src/main/java/chess/Bishop.java
@@ -17,69 +17,130 @@ public class Bishop implements PieceMovesCalculator {
             return moves;
         }
 
-        int zeroRow = position.getRow() - 1;
-        int zeroColumn = position.getColumn() - 1;
+        int zeroRowUpRight = position.getRow() - 1;
+        int zeroColumnUpRight = position.getColumn() - 1;
 
         // checks all the moves going up and to the right
         int upRightRow = position.getRow() + 1;
         int upRightColumn = position.getColumn() + 1;
 
-        while(zeroRow >= 0 && zeroRow < 8 && zeroColumn >= 0 && zeroColumn < 8) {
-            ChessPosition zeroPosition = new ChessPosition(zeroRow, zeroColumn);
+        while(zeroRowUpRight >= 0 && zeroRowUpRight < 8 && zeroColumnUpRight >= 0 && zeroColumnUpRight < 8) {
+            ChessPosition zeroPositionUR = new ChessPosition(zeroRowUpRight, zeroColumnUpRight);
             ChessPosition upRightPosition = new ChessPosition(upRightRow, upRightColumn);
-            ChessPiece occupiedPiece = board.getPiece(zeroPosition);
-            ChessMove newMove = new ChessMove(position, upRightPosition, null);
+            ChessPiece occupiedPieceUR = board.getPiece(zeroPositionUR);
+            ChessMove newMoveUR = new ChessMove(position, upRightPosition, null);
 
-            if (occupiedPiece == null) {
-                moves.add(newMove);
+            if (occupiedPieceUR == null) {
+                moves.add(newMoveUR);
                 upRightRow++;
                 upRightColumn++;
 
-                zeroRow++;
-                zeroColumn++;
-            } else if (occupiedPiece == piece) {
-                zeroRow++;
-                zeroColumn++;
+                zeroRowUpRight++;
+                zeroColumnUpRight++;
+            } else if (occupiedPieceUR == piece) {
+                zeroRowUpRight++;
+                zeroColumnUpRight++;
             } else {
-                if (occupiedPiece.getTeamColor() != myColour) {
-                    moves.add(newMove);
+                if (occupiedPieceUR.getTeamColor() != myColour) {
+                    moves.add(newMoveUR);
                 }
                 break;
             }
-            System.out.println("Moves: " + moves);
         }
 
         // checks all the moves going down and to the right
-        int zeroRow1 = position.getRow() - 1;
-        int zeroColumn1 = position.getColumn() - 1;
+        int zeroRowDownRight = position.getRow() - 1;
+        int zeroColumnDownRight = position.getColumn() - 1;
 
         int downRightRow = position.getRow() - 1;
         int downRightColumn = position.getColumn() + 1;
 
-        while(zeroRow1 > 0 && zeroRow1 < 8 && zeroColumn1 > 0 && zeroColumn1 < 8) {
-            ChessPosition zeroPosition = new ChessPosition(zeroRow1, zeroColumn1);
+        while(zeroRowDownRight > 0 && zeroRowDownRight < 8 && zeroColumnDownRight > 0 && zeroColumnDownRight < 8) {
+            ChessPosition zeroPositionDR = new ChessPosition(zeroRowDownRight, zeroColumnDownRight);
             ChessPosition downRightPosition = new ChessPosition(downRightRow, downRightColumn);
-            ChessPiece occupiedPiece = board.getPiece(zeroPosition);
-            ChessMove newMove = new ChessMove(position, downRightPosition, null);
+            ChessPiece occupiedPieceDR = board.getPiece(zeroPositionDR);
+            ChessMove newMoveDR = new ChessMove(position, downRightPosition, null);
 
-            if (occupiedPiece == null) {
-                moves.add(newMove);
+            if (occupiedPieceDR == null) {
+                moves.add(newMoveDR);
                 downRightRow--;
                 downRightColumn++;
 
-                zeroRow1--;
-                zeroColumn1++;
-            } else if (occupiedPiece == piece) {
-                zeroRow1++;
-                zeroColumn1++;
+                zeroRowDownRight--;
+                zeroColumnDownRight++;
+            } else if (occupiedPieceDR == piece) {
+                zeroRowDownRight--;
+                zeroColumnDownRight++;
             } else {
-                if (occupiedPiece.getTeamColor() != myColour) {
-                moves.add(newMove);
+                if (occupiedPieceDR.getTeamColor() != myColour) {
+                    moves.add(newMoveDR);
                 }
-            break;
+                break;
             }
         }
-        System.out.println("Moves: " + moves);
+
+        // checks all the moves going down and to the left
+        int zeroRowDownLeft = position.getRow() - 1;
+        int zeroColumnDownLeft = position.getColumn() - 1;
+
+        int downLeftRow = position.getRow() - 1;
+        int downLeftColumn = position.getColumn() - 1;
+
+        while(zeroRowDownLeft > 0 && zeroRowDownLeft < 8 && zeroColumnDownLeft > 0 && zeroColumnDownLeft < 8) {
+            ChessPosition zeroPositionDL = new ChessPosition(zeroRowDownLeft, zeroColumnDownLeft);
+            ChessPosition downLeftPosition = new ChessPosition(downLeftRow, downLeftColumn);
+            ChessPiece occupiedPieceDL = board.getPiece(zeroPositionDL);
+            ChessMove newMoveDL = new ChessMove(position, downLeftPosition, null);
+
+            if (occupiedPieceDL == null) {
+                moves.add(newMoveDL);
+                downLeftRow--;
+                downLeftColumn--;
+
+                zeroRowDownLeft--;
+                zeroColumnDownLeft--;
+            } else if (occupiedPieceDL == piece) {
+                zeroRowDownLeft--;
+                zeroColumnDownLeft--;
+            } else {
+                if (occupiedPieceDL.getTeamColor() != myColour) {
+                    moves.add(newMoveDL);
+                }
+                break;
+            }
+        }
+
+        // checks all the moves going up and to the left
+        int zeroRowUpLeft = position.getRow() - 1;
+        int zeroColumnUpLeft = position.getColumn() - 1;
+
+        int upLeftRow = position.getRow() + 1;
+        int upLeftColumn = position.getColumn() - 1;
+
+        while(zeroRowUpLeft > 0 && zeroRowUpLeft < 8 && zeroColumnUpLeft > 0 && zeroColumnUpLeft < 8) {
+            ChessPosition zeroPositionUL = new ChessPosition(zeroRowUpLeft, zeroColumnUpLeft);
+            ChessPosition upLeftPosition = new ChessPosition(upLeftRow, upLeftColumn);
+            ChessPiece occupiedPieceUL = board.getPiece(zeroPositionUL);
+            ChessMove newMoveUL = new ChessMove(position, upLeftPosition, null);
+
+            if (occupiedPieceUL == null) {
+                moves.add(newMoveUL);
+                upLeftRow++;
+                upLeftColumn--;
+
+                zeroRowUpLeft++;
+                zeroColumnUpLeft--;
+            } else if (occupiedPieceUL == piece) {
+                zeroRowUpLeft++;
+                zeroColumnUpLeft--;
+            } else {
+                if (occupiedPieceUL.getTeamColor() != myColour) {
+                    moves.add(newMoveUL);
+                }
+                break;
+            }
+        }
+
         return moves;
     }
 }
