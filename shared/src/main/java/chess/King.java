728e4995c06f2914466ec87fd10c5e068e3acd29
alyseswenson <alyse.swenson@gmail.com>
2025-01-26 14:11:16 -0700

    wrote the javadoc notes for King

--- shared/src/main/java/chess/King.java
+++ shared/src/main/java/chess/King.java
@@ -3,7 +3,34 @@ package chess;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * This class implements the PieceMovesCalculator interface. It compiles a list of all the possible moves a king
+ * can make on its turn.
+ * <p>
+ * Kings can move in any direction, but only one place per turn. They can take a piece by moving onto its square.
+ */
 public class King implements PieceMovesCalculator{
+
+    /**
+     * Here is where the moves are calculated.
+     * <p>
+     * It is important to know what team the piece being checked is on.
+     * <p>
+     * Kings can move in any direction. Directly up (0,+), up and to the right (+,+), directly right (+,0),
+     * down and to the right (+,-), directly down (0,-), down and to the left (-,-), directly left (-,0),
+     * and up and to the left (-,+). It helps to think about it as a coordinate plane.
+     * <p>
+     * Before any move is made, you want to see if that spot is empty or not. If it is, the king can move to that spot.
+     * If it is not empty, that means that the king either cannot go there (if it's his team) or can take the piece's spot.
+     * <p>
+     * Remember that the list of moves will only let you add a ChessMove type to it, and that a ChessMove
+     * needs a ChessPosition to be initialised.
+     * <p>
+     * Make sure to check every move the king can make.
+     * @param position
+     * @param board
+     * @return a list of all the possible moves a king can make
+     */
     public static List<ChessMove> possibleMoves(ChessPosition position, ChessBoard board) {
         List<ChessMove> moves = new ArrayList<>();
 
