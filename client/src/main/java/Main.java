13c4a81100917eb25ee3e85f2a96de2cce3f2185
alyseswenson <alyse.swenson@gmail.com>
2025-03-21 13:03:59 -0600

    started implementing ui

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,8 +1,19 @@
 import chess.*;
+import ui.DisplayBoard;
+
+import java.util.Scanner;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Random;
+
+import static ui.EscapeSequences.*;
 
 public class Main {
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Client: " + piece);
+//        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+//        System.out.println("♕ 240 Chess Client: " + piece);
+
+        DisplayBoard.main();
+
     }
 }
