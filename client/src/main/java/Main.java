ee1aaf0ebf15af9cfd0ce562418e941353178754
alyseswenson <alyse.swenson@gmail.com>
2025-03-22 18:20:20 -0600

    starting this section over :(

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,19 +1,7 @@
-import chess.*;
-import ui.DisplayBoard;
-
-import java.util.Scanner;
-import java.io.PrintStream;
-import java.nio.charset.StandardCharsets;
-import java.util.Random;
-
-import static ui.EscapeSequences.*;
-
 public class Main {
     public static void main(String[] args) {
 //        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
 //        System.out.println("♕ 240 Chess Client: " + piece);
-
-        DisplayBoard.main();
 
     }
 }
