8fc259d4289c7b8c4d8017b6d49ba1988e8a30c9
alyseswenson <alyse.swenson@gmail.com>
2025-03-22 20:13:25 -0600

    updated postlogin ui with more colours and parsing the user input a bit better

--- client/src/main/java/ui/PostLoginUI.java
+++ client/src/main/java/ui/PostLoginUI.java
@@ -5,32 +5,75 @@ import java.io.PrintStream;
 public class PostLoginUI {
 
     public void displayHelp(PrintStream out){
-        out.println(EscapeSequences.SET_TEXT_COLOR_BLUE + "logout" + EscapeSequences.RESET_TEXT_COLOR + " -> log out of your account"); // Blue text
-        out.println(EscapeSequences.SET_TEXT_COLOR_BLUE + "create <NAME>" + EscapeSequences.RESET_TEXT_COLOR + " -> create a new chess game");
-        out.println(EscapeSequences.SET_TEXT_COLOR_BLUE + "join <ID> [WHITE|BLACK]" + EscapeSequences.RESET_TEXT_COLOR + " -> list all games");
-        out.println(EscapeSequences.SET_TEXT_COLOR_BLUE + "observe <ID>" + EscapeSequences.RESET_TEXT_COLOR + " -> watch a game");
-        out.println(EscapeSequences.SET_TEXT_COLOR_BLUE + "help" + EscapeSequences.RESET_TEXT_COLOR + " -> list possible commands");
+        out.println(EscapeSequences.SET_TEXT_COLOR_BLUE + "logout" +
+                EscapeSequences.RESET_TEXT_COLOR + " -> log out of your account");
+        out.println(EscapeSequences.SET_TEXT_COLOR_BLUE + "create <NAME>" +
+                EscapeSequences.RESET_TEXT_COLOR + " -> create a new chess game");
+        out.println(EscapeSequences.SET_TEXT_COLOR_BLUE + "join <ID> [WHITE|BLACK]" +
+                EscapeSequences.RESET_TEXT_COLOR + " -> join specific game as the white or black player");
+        out.println(EscapeSequences.SET_TEXT_COLOR_BLUE + "list" +
+                EscapeSequences.RESET_TEXT_COLOR + " -> list all games");
+        out.println(EscapeSequences.SET_TEXT_COLOR_BLUE + "observe <ID>" +
+                EscapeSequences.RESET_TEXT_COLOR + " -> watch a game");
+        out.println(EscapeSequences.SET_TEXT_COLOR_BLUE + "help" +
+                EscapeSequences.RESET_TEXT_COLOR + " -> list possible commands");
     }
 
     public void postLoginCommandHandler(PrintStream out, String command) {
+        String[] partsOfCommand = command.split(" ");
 
-        String lowerCase = command.toLowerCase();
+        if (partsOfCommand.length == 0) {
+            out.println(EscapeSequences.SET_TEXT_COLOR_BLUE + "Invalid command. Type help for a list of commands." + EscapeSequences.RESET_TEXT_COLOR);
+        }
 
-        if (lowerCase.contains("logout")) {
+        String actualCommand = partsOfCommand[0].toLowerCase();
+
+        if (actualCommand.equals("logout") && partsOfCommand.length == 1) {
             out.println(EscapeSequences.SET_TEXT_COLOR_BLUE + "Logging out. " + EscapeSequences.RESET_TEXT_COLOR);
-        } else if (command.equalsIgnoreCase("create")) {
-            out.println(EscapeSequences.SET_TEXT_COLOR_BLUE + "Create a new chess game: " + EscapeSequences.RESET_TEXT_COLOR);
-        } else if (lowerCase.contains("join")){
-                out.println(EscapeSequences.SET_TEXT_COLOR_BLUE + "Joining game as player." + EscapeSequences.RESET_TEXT_COLOR);
-        } else if (lowerCase.contains("observe")){
-            out.println(EscapeSequences.SET_TEXT_COLOR_BLUE + "Joining game as observer." + EscapeSequences.RESET_TEXT_COLOR);
-        } else if (command.equalsIgnoreCase("quit")) {
+
+
+
+        } else if (command.equals("create") && partsOfCommand.length == 2) {
+
+            String gameName = partsOfCommand[1];
+            out.println(EscapeSequences.SET_TEXT_COLOR_BLUE + "Creating new chess game: " + EscapeSequences.SET_TEXT_COLOR_YELLOW + gameName + EscapeSequences.RESET_TEXT_COLOR);
+
+
+
+
+        } else if (actualCommand.equals("join") && partsOfCommand.length == 3){
+            String id = partsOfCommand[1];
+            String playerColour = partsOfCommand[2];
+
+            out.println(EscapeSequences.SET_TEXT_COLOR_BLUE + "Joining " +
+                    EscapeSequences.SET_TEXT_COLOR_YELLOW + id + EscapeSequences.RESET_TEXT_COLOR + " as " +
+                    EscapeSequences.SET_TEXT_COLOR_YELLOW + playerColour + " player." + EscapeSequences.RESET_TEXT_COLOR);
+
+
+
+        } else if (actualCommand.equals("observe") && partsOfCommand.length == 2){
+            String id = partsOfCommand[1];
+
+            out.println(EscapeSequences.SET_TEXT_COLOR_BLUE + "Joining " + id +
+                    " as observer." + EscapeSequences.RESET_TEXT_COLOR);
+
+
+
+
+        } else if (actualCommand.equals("quit") && partsOfCommand.length == 1) {
             out.println(EscapeSequences.SET_TEXT_COLOR_BLUE + "Exiting program." + EscapeSequences.RESET_TEXT_COLOR);
             System.exit(0);
-        } else if (command.equalsIgnoreCase("help")) {
+
+
+
+
+        } else if (actualCommand.equals("help") && partsOfCommand.length == 1) {
             displayHelp(out);
+
         } else {
-            out.println(EscapeSequences.SET_TEXT_COLOR_BLUE + "Invalid command. Type help for a list of commands." + EscapeSequences.RESET_TEXT_COLOR);
+            out.println(EscapeSequences.SET_TEXT_COLOR_BLUE + "Invalid command. Type " +
+                    EscapeSequences.SET_TEXT_COLOR_YELLOW + "help" + " for a list of commands." +
+                    EscapeSequences.RESET_TEXT_COLOR);
         }
     }
 
