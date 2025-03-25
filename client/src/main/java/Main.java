e89ba3e3b8ba9f66b72975be692f3d78112285a7
alyseswenson <alyse.swenson@gmail.com>
2025-03-25 11:06:23 -0600

    implented repl again

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -7,14 +7,11 @@ public class Main {
     public static void main(String[] args) {
         var serverURL = "http://localhost:8080";
 
-//        if (args.length == 1) {
-//            serverURL = args[0];
-//        }
-//
-//        new Repl(serverURL).run();
-        ChessGame newGame = new ChessGame();
-        GameBoardDrawing.drawBoard(ChessGame.TeamColor.WHITE, newGame.getBoard());
-        GameBoardDrawing.drawBoard(ChessGame.TeamColor.BLACK, newGame.getBoard());
+        if (args.length == 1) {
+            serverURL = args[0];
+        }
+
+        new Repl(serverURL).run();
 
     }
 }
