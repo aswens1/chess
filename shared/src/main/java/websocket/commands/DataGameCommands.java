8e47195bf9917c6be6cbcad5f2b85e8f140c6b6f
alyseswenson <alyse.swenson@gmail.com>
2025-04-11 20:15:57 -0600

    deleted unused class

--- shared/src/main/java/websocket/commands/DataGameCommands.java
+++
@@ -1,4 +0,0 @@
-package websocket.commands;
-
-public record DataGameCommands(websocket.commands.UserGameCommand.CommandType commandType, String authToken,
-                               Integer gameID, String playerColor, String username, String ogPos, String newPos) {}
